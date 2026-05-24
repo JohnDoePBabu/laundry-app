7ece24feec8b7f42524abfe010e4617e283063e8
JohnDoePBabu <s.shreyaz1@gmail.com>
2026-05-24 15:27:54 +0530

    [] change prisma client

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../generated/prisma/client.ts'
+import { PrismaClient } from '@prisma/client'
 // A single PrismaClient instance is reused across the entire process.
 // Creating one per request would open a new connection pool each time
 // and quickly exhaust the Postgres max_connections limit.
